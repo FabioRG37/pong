7dfac5cf2ab883651e4b78212e2cfe55d97082a6
FabioRG37 <fabio_richardfrg@hotmail.com>
2025-01-23 18:45:16 -0300

    Implementação básica de movimentos e colisão do jogador um

--- src/Window.java
+++ src/Window.java
@@ -1,17 +1,81 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Window extends JFrame implements Runnable {
+
+    public Graphics2D g2;
+    public KL keyListener = new KL();
+    public Rect playOne, ai, ball;
+    public PlayerController playerController;
+
     public Window(){
-        this.setSize(800, 600);
-        this.setTitle("Pong");
+        this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+        this.setTitle(Constants.SCREEN_TITLE);
         this.setResizable(false);
-        this.setVisible( true);
+        this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(keyListener);
+        Constants.TOOLBAR_HEIGHT = this.getInsets().top;
+        Constants.INSETS_BOTTOM = this.getInsets().bottom;
+
+        g2 = (Graphics2D)this.getGraphics();
+
+        playOne = new Rect(
+                Constants.HZ_PADDING,
+                40,
+                Constants.PADDLE_WIDTH,
+                Constants.PADDLE_HEIGHT,
+                Constants.PADDLE_COLOR);
+        playerController = new PlayerController(playOne, keyListener);
+
+        ai = new Rect(
+                Constants.SCREEN_WIDTH - Constants.PADDLE_WIDTH - Constants.HZ_PADDING,
+                40,
+                Constants.PADDLE_WIDTH,
+                Constants.PADDLE_HEIGHT,
+                Constants.PADDLE_COLOR
+        );
+        ball = new Rect(
+                Constants.SCREEN_WIDTH / 2,
+                Constants.SCREEN_HEIGHT / 2,
+                Constants.BALL_WIDTH,
+                Constants.BALL_WIDTH,
+                Constants.PADDLE_COLOR
+        );
+
     }
+
+    public void update(double dt) {
+        Image dbImage = createImage(getWidth(), getHeight());
+        Graphics dbg = dbImage.getGraphics();
+        this.draw(dbg);
+        g2.drawImage(dbImage, 0, 0, this);
+
+        playerController.update(dt);
+    }
+
+    public void draw(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+
+        playOne.draw(g2);
+        ai.draw(g2);
+        ball.draw(g2);
+    }
+
     @Override
     public void run() {
+        double lastFrameTime = 0.0;
         while (true) {
-            // do whatever
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
+
+            update(deltaTime);
+
         }
     }
 }
